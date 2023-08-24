034cb3276cdbe7dd8cb56ef9d3f83fe3be0d5357
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2023-08-24 08:39:19 +0700

    Add Header

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,11 +1,12 @@
-const Button = ({ children, className }) => {
-  return (
-    <button
-      className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center ${className}`}
-    >
-      {children}
-    </button>
-  );
+const Button = ({ children, className, ...props }) => {
+    return (
+        <button
+            className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center ${className}`}
+            {...props}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,4 +1,5 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 import Button from "../../components/Button";
 import Input from "../../components/login/Input";
 import { Link, useNavigate } from "react-router-dom";
@@ -6,66 +7,107 @@ import AuthService from "../../services/authService";
 
 const authService = new AuthService();
 
-const RegisterPage = () => {
-    const navigate = useNavigate();
-
-    const { handleSubmit, handleChange, values } = useFormik({
-        initialValues: {
-            fullname: "",
-            email: "",
-            password: "",
-        },
-        onSubmit: async (values) => {
-            const register = await authService.register({
-                fullname: values.fullname || "",
-                email: values.email || "",
-                password: values.password || "",
+const formikConfig = {
+    initialValues: {
+        fullname: "",
+        password: "",
+        confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+        fullname: Yup.string().required("Full name is required"),
+        password: Yup.string().required("Password is required"),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref("password"), null], "Passwords must match")
+            .required("Confirm password is required"),
+    }),
+    onSubmit: async (values, { setSubmitting, setErrors }) => {
+        try {
+            // const register = await authService.register({
+            //     fullname: values.fullname || "",
+            //     password: values.password || "",
+            // });
+            // if (register.status === 200) {
+            //     alert("Đăng kí thành công");
+            // }
+            alert("Registered!");
+        } catch (error) {
+            setErrors({
+                submit: "Registration failed. Please try again later.",
             });
-            if (register.status === 200) {
-                alert("Đăng kí thành công");
-                navigate("/login");
-            }
-        },
-    });
+        } finally {
+            setSubmitting(false);
+        }
+    },
+};
+
+const RegisterPage = () => {
+    const {
+        handleSubmit,
+        handleChange,
+        values,
+        errors,
+        touched,
+        isSubmitting,
+    } = useFormik(formikConfig);
 
     return (
         <div className="flex justify-center items-center w-full h-screen bg-gray-100">
             <div className="w-[400px] bg-white rounded-lg p-6">
                 <form onSubmit={handleSubmit}>
                     <h1 className="font-bold text-xl text-center mb-4">
-                        Đăng ký
+                        Social Media App
                     </h1>
+                    {errors.submit && (
+                        <div className="text-red-500 mb-4">{errors.submit}</div>
+                    )}
                     <Input
                         type="text"
                         name="fullname"
-                        placeholder="Full name"
+                        placeholder="Username ..."
                         className="mb-4"
                         onChange={handleChange}
                         value={values.fullname}
-                    />
-                    <Input
-                        type="text"
-                        name="email"
-                        placeholder="Email"
-                        className="mb-4"
-                        onChange={handleChange}
-                        value={values.email}
+                        error={errors.fullname && touched.fullname}
+                        helperText={touched.fullname && errors.fullname}
                     />
                     <Input
                         type="password"
                         name="password"
-                        placeholder="Password"
+                        placeholder="Password ..."
                         className="mb-4"
                         onChange={handleChange}
                         value={values.password}
+                        error={errors.password && touched.password}
+                        helperText={touched.password && errors.password}
                     />
-                    <div className="mb-4">
-                        Bạn đã có tài khoản chưa,{" "}
-                        <Link to="/login" className="text-blue-500">
-                            Đăng nhập
+                    <Input
+                        type="password"
+                        name="confirmPassword"
+                        placeholder="Confirm Password ..."
+                        className="mb-4"
+                        onChange={handleChange}
+                        value={values.confirmPassword}
+                        error={
+                            errors.confirmPassword && touched.confirmPassword
+                        }
+                        helperText={
+                            touched.confirmPassword && errors.confirmPassword
+                        }
+                    />
+                    <div className="flex justify-center">
+                        <Button
+                            type="submit"
+                            className="w-30 text-black border-2 bg-transparent hover:text-black hover:bg-transparent hover:border-black"
+                            disabled={isSubmitting}
+                        >
+                            {isSubmitting ? "Registering..." : "Register"}
+                        </Button>
+                    </div>
+                    <div className="mb-4 mt-4 flex justify-center">
+                        <Link to="/login" className="text-blue-500 ">
+                            Back to login
                         </Link>
                     </div>
-                    <Button type="submit">Sign up</Button>
                 </form>
             </div>
         </div>
