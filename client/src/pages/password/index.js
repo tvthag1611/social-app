ddcc8efaf18631e91f36639d81eb759cb1996334
Thắng <thang.tran5@onemount.com>
2023-08-30 21:24:08 +0700

    fix: error import

--- client/src/pages/password/index.js
+++ client/src/pages/password/index.js
@@ -1,12 +1,11 @@
+import { Button, Input } from "antd";
 import { useFormik } from "formik";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Input from "../../components/login/Input";
-import Button from "../../components/Button";
 
 const PassWord = () => {
   const navigate = useNavigate();
-//   const { handleLogin } = useContext(AuthContext);
+  //   const { handleLogin } = useContext(AuthContext);
 
   const { handleSubmit, handleChange, values } = useFormik({
     initialValues: {
@@ -15,18 +14,18 @@ const PassWord = () => {
       confirmPassword: "",
     },
     onSubmit: async (values) => {
-    //   try {
-    //     const response = await AuthService.login(values);
-    //     if (response.status === 200) {
-    //       localStorage.setItem("accessToken", response?.data?.accessToken);
-    //       await handleLogin(values);
-    //       alert("Đăng nhập thành công");
-    //       navigate("/");
-    //     }
-    //   } catch (error) {
-    //     alert(error.response.data.message);
-    //   }
-    console.log(values)
+      //   try {
+      //     const response = await AuthService.login(values);
+      //     if (response.status === 200) {
+      //       localStorage.setItem("accessToken", response?.data?.accessToken);
+      //       await handleLogin(values);
+      //       alert("Đăng nhập thành công");
+      //       navigate("/");
+      //     }
+      //   } catch (error) {
+      //     alert(error.response.data.message);
+      //   }
+      console.log(values);
     },
   });
   return (
@@ -34,7 +33,9 @@ const PassWord = () => {
       <div className="flex justify-center items-center w-full h-screen bg-gray-100">
         <div className="w-[400px] bg-white rounded-lg p-6">
           <form onSubmit={handleSubmit}>
-            <h1 className="font-bold text-xl text-center mb-4">Thay đổi mật khẩu</h1>
+            <h1 className="font-bold text-xl text-center mb-4">
+              Thay đổi mật khẩu
+            </h1>
             <Input
               type="password"
               name="Current Password"
