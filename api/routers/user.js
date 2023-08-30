495dbb0f3f2fb9704e3a72692ac7a1731db4ec8f
NathanCao0111 <nhatcao0111@gmail.com>
2023-08-31 00:00:56 +0700

    add backend API update MyInfo & config cloudinary

--- api/routers/user.js
+++ api/routers/user.js
@@ -6,13 +6,16 @@ import {
   getById,
   updateUser,
   uploadAvatarController,
+  updateInfo,
 } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.post("/upload", uploadFile.single("avatar"), uploadAvatarController);
 userRouter.post("/create", createUser);
 userRouter.post("/update", updateUser);
+userRouter.put("/update/info", authMiddleware, updateInfo);
 userRouter.get("/getall", getAll);
 userRouter.get("/getbyid", getById);
 
